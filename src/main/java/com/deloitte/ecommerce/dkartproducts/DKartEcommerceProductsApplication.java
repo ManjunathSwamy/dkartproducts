6298e982ec84ab2b0523add73d7b0c8081d4d2a9
ManjunathSwamy <manjusoft8@gmail.com>
2020-02-29 00:32:50 +0530

    Update DKartEcommerceProductsApplication.java

--- src/main/java/com/deloitte/ecommerce/dkartproducts/DKartEcommerceProductsApplication.java
+++ src/main/java/com/deloitte/ecommerce/dkartproducts/DKartEcommerceProductsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
